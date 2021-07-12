5a49ad4a85484f1948d202a1ac64428d139cc5e2
Eleven-Twelv-e <nicat.maharramov.2020@gmail.com>
2021-07-12 22:17:56 +0400

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+       Ints calc = new İntsCalculator();
+        System.out.println(calc.mult(3,2));
+        System.out.println(calc.pow(34,45));
+        System.out.println(calc.sum(12,78));
+    }
+}
